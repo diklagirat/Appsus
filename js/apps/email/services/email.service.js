919e234cdc82f2bcc2dba758c81403fd8647ae87
efratRotem <ma.efrat@gmail.com>
2022-06-29 21:36:43 +0300

    setting gitignore

---
+++ js/apps/email/services/email.service.js
@@ -0,0 +1,39 @@
+import { storageService } from "../../../services/storage.service.js"
+import { utilService } from "../../../services/util.service.js"
+import emailJson from '../../../../data' assert {type: "json"}
+
+const NOTES_KEY = 'notes'
+_createNotes()
+
+export const noteService = {
+    query,
+    get,
+    remove,
+    save,
+}
+
+function query() {
+    return storageService.query(NOTES_KEY);
+}
+
+function _createNotes() {
+    let notes = utilService.loadFromStorage(NOTES_KEY);
+    if (!notes || !notes.length) {
+        notes = notesJson;
+        utilService.saveToStorage(NOTES_KEY, notes);
+    }
+    return notes;
+}
+
+function get(noteId) {
+    return storageService.get(NOTES_KEY, noteId)
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTES_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) return storageService.put(NOTES_KEY, note)
+    else return storageService.post(NOTES_KEY, note)
+}
