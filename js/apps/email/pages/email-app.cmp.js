e24be46afc7766e7864d582fe1b2766551f52218
efratRotem <ma.efrat@gmail.com>
2022-07-01 17:10:34 +0300

    filter by read/ unread

--- js/apps/email/pages/email-app.cmp.js
+++ js/apps/email/pages/email-app.cmp.js
@@ -2,6 +2,7 @@ import { emailService } from '../services/email.service.js'
 import emailList from '../cmp/email-list.cmp.js'
 import emailFolderList from '../cmp/email-folder-list.cmp.js'
 import addEmail from '../cmp/add-email.cmp.js'
+import emailFilter from '../cmp/email-filter.cmp.js'
 
 //TODO: Fix newEmail date presentation
 // TODO: Show last email as the first email
@@ -10,6 +11,7 @@ export default {
     template: `
         <section class="email-app">
             <button @click="composeEmail" ><i class="fas fa-plus"></i> Compose</button>
+            <email-filter @filtered="setFilter"/>
             <add-email v-if="isComposedEmail" @added="addEmail"/>
             <email-folder-list :emails="emailsInInbox"/>
             <email-list :emails="emailsToShow"/>
@@ -18,7 +20,8 @@ export default {
     components: {
         emailList,
         emailFolderList,
-        addEmail
+        addEmail,
+        emailFilter,
     },
     data() {
         return {
@@ -27,6 +30,7 @@ export default {
             userEmail: 'user@appsus.com',
             nextId: 105,
             newEmail: null,
+            filterBy: null,
         }
     },
     created() {
@@ -42,8 +46,8 @@ export default {
             this.isComposedEmail = !this.isComposedEmail
             console.log('Compose email')
         },
-        addEmail(email){
-            console.log('email:',email)
+        addEmail(email) {
+            console.log('email:', email)
             this.newEmail = {
                 subject: email.subject,
                 body: email.body,
@@ -51,17 +55,27 @@ export default {
                 sentAt: new Date().toDateString(),
                 to: email.to
             }
-    
+
             console.log('adding email', this.newEmail)
             this.emails.unshift(this.newEmail)
-            console.log('this.emails:',this.emails)
+            console.log('this.emails:', this.emails)
             emailService.save(this.newEmail)
+        },
+        setFilter(filterBy) {
+            this.filterBy = filterBy
         }
     },
     computed: {
-     
         emailsToShow() {
-            return this.emails
+            var emails = this.emails
+            if (this.filterBy) {
+                if (this.filterBy === 'Read') {
+                    emails = emails.filter(email => email.isRead === true)
+                } else if (this.filterBy === 'Unread') {
+                    emails = emails.filter(email => email.isRead === false)
+                }
+            }
+            return emails
         },
         emailsInInbox() {
             if (!this.emails || !this.emails.length) return this.emails
